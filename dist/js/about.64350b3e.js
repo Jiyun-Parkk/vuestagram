d575c7fbfac2f6bfff3c76296edb22f2cc588ad6
박지윤 <jyun1937@naver.com>
2022-08-23 18:08:33 +0900

    add dist file

---
+++ dist/js/about.64350b3e.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvuestagram"]=self["webpackChunkvuestagram"]||[]).push([[443],{381:function(a,n,u){u.r(n),u.d(n,{default:function(){return l}});var t=u(252);const e={class:"about"},r=(0,t._)("h1",null,"This is an about page",-1),s=[r];function c(a,n){return(0,t.wg)(),(0,t.iD)("div",e,s)}var i=u(744);const o={},f=(0,i.Z)(o,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=about.64350b3e.js.map
